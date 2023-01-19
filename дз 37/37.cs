412f1f2487cb9df2f7d7f7b247e144edef6a5050
marina140291 <marina140291@mail.ru>
2023-01-19 23:49:28 +0300

    37

---
+++ дз 37/37.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void input_array(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+         array[i]=new Random().Next(1, 10);
+}
+
+void release(int[]array)
+{
+int Max = array[0];
+int Min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+    if (Max < array[i])
+         Max = array[i];
+    else   
+       if (Min > array[i])
+       Min = array[i];
+
+    Console.WriteLine(Max);
+    Console.WriteLine( Min);
+    Console.WriteLine(Max - Min);
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]array=new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(" ,  ", array)}]");
+release(array);
