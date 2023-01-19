67f3795b89e40e953299e5abaeb8dca52a578a68
marina140291 <marina140291@mail.ru>
2023-01-19 22:03:50 +0300

    36

---
+++ дз 36/36.cs
@@ -0,0 +1,24 @@
+﻿// задайте одномерный массив, заполненный случайными числами.
+// найдите сумму элементов находящихся на нечетных позициях
+
+void input_array(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+         array[i]=new Random().Next(1, 10);
+}
+
+void release(int[]array)
+{int sum = 0;
+for (int i = 0; i < array.Length; i = i + 2)
+         sum += array[i];
+    Console.WriteLine(sum);
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]array=new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(" ,  ", array)}]");
+release(array);
